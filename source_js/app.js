d3fbd4100e5079906e363472e7d69d71e0ff3b2e
Siddhanth Maheshwari <siddhanthm@outlook.com>
2016-03-05 19:11:43 -0600

    retry

--- source_js/app.js
+++ source_js/app.js
@@ -2,13 +2,13 @@ var app = angular.module('mp3',['ngRoute','movieControllers']);
 
 app.config(function ($routeProvider) {
 	$routeProvider.when('/list', {
-		templateUrl: './../partials/list.html',
+		templateUrl: './partials/list.html',
 		controller: 'ListController'
 	}).when('/details/:itemID', {
-		templateUrl: './../partials/details.html',
+		templateUrl: './partials/details.html',
 		controller: 'DetailController'
 	}).when('/gallery', {
-		templateUrl: './../partials/gallery.html',
+		templateUrl: './partials/gallery.html',
 		controller: 'GalleryController'
 	}).otherwise({ redirectTo: '/list' });
 });
